3378745243438a6720639a4f19e1ec791b745693
Brad Greenlee <brad@wesabe.com>
2010-03-27 23:34:01 -0700

    Add some error handling.

--- app.rb
+++ app.rb
@@ -46,9 +46,15 @@ end
 
 # store the request tokens and send to Twitter
 get '/connect' do
-  request_token = @client.request_token(
-    :oauth_callback => @@config['callback_url']
-  )
+  begin
+    request_token = @client.request_token(
+      :oauth_callback => @@config['callback_url']
+    )
+  rescue Errno::ECONNRESET => e
+    @error = "There was a problem connecting to Twitter. Please try again."
+    redirect '/'
+  end
+  
   session[:request_token] = request_token.token
   session[:request_token_secret] = request_token.secret
   redirect request_token.authorize_url.gsub('authorize', 'authenticate') 
@@ -65,8 +71,9 @@ get '/auth' do
       session[:request_token_secret],
       :oauth_verifier => params[:oauth_verifier]
     )
-  rescue OAuth::Unauthorized
-    # TODO: error handling
+  rescue OAuth::Unauthorized, Errno::ECONNRESET => e
+    @error = "There was a problem connecting to Twitter. Please try again."
+    redirect '/'
   end
 
   if @client.authorized? && @access_token
