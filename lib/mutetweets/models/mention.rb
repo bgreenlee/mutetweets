4899a000c1d42bc545ee62de0fcc8d1b20e6f767
Brad Greenlee <brad@etsy.com>
2014-10-06 22:01:28 -0700

    Fix deprecations

--- lib/mutetweets/models/mention.rb
+++ lib/mutetweets/models/mention.rb
@@ -1,6 +1,6 @@
 class Mention < Tweet
   def initialize(params)
-    @muter = params["user"]["screen_name"]
+    @muter = params.user.screen_name
     super
     parse!
   end
@@ -9,7 +9,7 @@ class Mention < Tweet
   def parse!
     if m = /@mutetweets\s+@?(\w+)\s+(?:for)?\s*(\d+)(m|h|d)(?:\s+-(v|verbose)\b)?/i.match(@text)
       @mutee = m[1]
-      @length = m[2].to_i * UNITS[m[3]]
+      @length = m[2].to_i * UNITS[m[3].downcase]
       @verbose = %w{v verbose}.include?(m[4])
     end
   end
--- lib/mutetweets/models/tweet.rb
+++ lib/mutetweets/models/tweet.rb
@@ -8,14 +8,14 @@ class Tweet
   UNITS = {'m' => 60, 'h' => 3600, 'd' => 86400}
 
   def initialize(params)
-    @id = params["id"]
+    @id = params.id
     begin
-      @created_at = params["created_at"].kind_of?(Time) ? params["created_at"] : Time.parse(params["created_at"])
+      @created_at = params.created_at.kind_of?(Time) ? params.created_at : Time.parse(params.created_at)
     rescue Exception => e
-      $stderr.puts("invalid time: #{params['created_at']}\nError: #{e.message}")
+      $stderr.puts("invalid time: #{params.created_at}\nError: #{e.message}")
       @created_at = nil
     end
-    @text = params["text"]
+    @text = params.text
     @mutee = nil
     @length = nil
     @direct_message = false
@@ -25,11 +25,11 @@ class Tweet
   def valid_mute?
     @created_at && @mutee && @length
   end
-  
+
   def direct_message?
     direct_message
   end
-  
+
   def to_s
     "[#{@id}] #{@muter}: #{@text}"
   end
